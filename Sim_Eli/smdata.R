7d775c7ac7a9ef4d1d88875deb2b01996890425c
Eli-Boaz Clapper <74557493+Hypergeometricdistribution@users.noreply.github.com>
2021-12-07 15:59:51 +0100

    update smdata.R

--- Sim_Eli/smdata.R
+++ Sim_Eli/smdata.R
@@ -2,9 +2,14 @@
 simdata <- function(es,var_n, n){
   var_n <- var_n + 1 #to include outcome variable in mvrnorm()
   M <- rep(0,var_n) #set mean to 0 for identification
-  S <- diag(var_n) * 1 #predictors have correlations of 1 with themselves
-  S[,1][-1] <- es #correlations with outcome variable
+  
+  # 1 can be replaced with some value for the desired variance of the outcome and predictors.
+  S <- diag(var_n) * 1 #predictors have variances of 1
   #diag(S)[-1] <- tau2 #something like this could add some more noise to the predictors
+  
+  #this now sets all covariances s_y, s_xi to the same value. we can add some variability in the covariances
+  S[,1][-1] <- es #covariances with outcome variable 
+  
   df <-  mvrnorm(n, M, S, tol = 1E-6, empirical = F) #generate data
   colnames(df) <- c('y', sprintf("X%02d", 1:(var_n-1))) #give names
   return(df)
