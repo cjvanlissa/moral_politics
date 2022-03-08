72b26eb43159cdf97d5e59f2c6786447709126ae
Eli-Boaz Clapper <74557493+EliClapper@users.noreply.github.com>
2022-03-08 18:14:05 +0100

    add prelim results

--- Sim_Eli/Simulatie_EliV2.R
+++ Sim_Eli/Simulatie_EliV2.R
@@ -1,5 +1,6 @@
 #clear console and env
 rm(list=ls(all.names = T))
+gc(reset = T, full = T)
 cat("\014")
 
 #load necessary packages
@@ -14,48 +15,54 @@ versions <- c(
 if(!all(versions == 0)) stop("Using the incorrect version of one or more packages.")
 
 # Load simulation functions from source -----------------------------------
-source('./Sim_Eli/functions.R')
+source('Sim_Eli/functions.R')
 
 #set conditions for simulation
+nreps <- 50 #can be changed to hundred
 hyper_parameters<-list(
-  es = c(0, 0.1),       #true effect size = true correlation with outcome
-  tau2 = c(1,4),       #reliability of measurement (variance of measurements)
-  n = c(100,500),       #sample size (can be divided by I to obtain different sample sizes per group )
-  k = c(3, 10),         #number of groups
-  hyp_val = c(0,0.1)   #thresholds for informative hypotheses
+  ndataset = 1:nreps,
+  es = c(0, 0.1, 0.5),       # true effect size = true correlation with outcome
+  tau2 = c(1, 4),             # reliability of outcome (standard deviation of outcome)
+  n = c(100, 500),            # mean sample size 
+  k = c(3, 10),           # number of groups
+  hyp_val = c(0, 0.1, 0.5)     # thresholds for informative hypotheses
 )
 
 # Create grid with simulation parameters and save it
 summarydata <- expand.grid(hyper_parameters, stringsAsFactors = FALSE)
-summarydata <- cbind(V1 = 1:nrow(summarydata), summarydata)
 
 saveRDS(summarydata, file = "./Sim_Eli/summarydata.RData")
-summarydata<-readRDS("./Sim_Eli/summarydata.RData")
+# summarydata<-readRDS("./Sim_Eli/summarydata.RData")
+
+
+# create folder to store results in and delete folder where previous results are in.
+unlink("./sim_Eli/Results", recursive = T)
+dir.create("./Sim_Eli/Results")
+
+# number of algorithms used
+nalg <- 6
 
 # prepare multicluster
+set.seed(6164900)
 library(doSNOW)
-nclust <- parallel::detectCores() - 1
+nclust <- parallel::detectCores() 
 cl <- makeCluster(nclust) 
 registerDoSNOW(cl) 
-
-nreps <- 3 #we replicate a condition n times
-nalg <- 6 # there are, as of now 6 algorithms: prodbf, tbf, gpbf, all having ic and iu
 
 # add progression bar
 pb <- txtProgressBar(min = 0, max = nrow(summarydata), style = 3)
 opts <- list(progress = function(n) setTxtProgressBar(pb, n))
 
 # export nalg and nreps and set seed over clusters
-clusterExport(cl, c('nalg', 'nreps'))
-clusterEvalQ(cl, set.seed(6164900)) #set seed for all processors.
+set.seed(6164900)
+seeds <- sample(1:.Machine$integer.max, nrow(summarydata))
+clusterExport(cl, c('nalg', 'seeds'))
 
-# create folder to store results in
-unlink("./sim_Eli/Results", recursive = T)
-dir.create("./Sim_Eli/Results")
 
 # run parallel computation
 foreach(rownum = 1:nrow(summarydata), .options.snow = opts, .packages = c("bain", "MASS")) %dopar% {
-  res <-  replicate(nreps,tryCatch(
+  set.seed(seeds[rownum])
+  res <-  tryCatch(
     c(rownum, 
       BFs(es = summarydata[rownum, "es"],
              n = summarydata[rownum, "n"],
@@ -63,13 +70,12 @@ foreach(rownum = 1:nrow(summarydata), .options.snow = opts, .packages = c("bain"
              k =summarydata[rownum, "k"],
              tau2 = summarydata[rownum, "tau2"])),
              error = function(e){c(rownum, rep(NA, nalg))}
-    ))
+    )
   
   # every cluster creates it own .txt file where the results are stored
   write.table(x = t(res), file = sprintf("./Sim_Eli/Results/results%d.txt" , Sys.getpid()), 
               sep = "\t", append = TRUE, row.names = FALSE, col.names = FALSE)
 }
-
 
 #Close cluster
 stopCluster(cl)
@@ -79,70 +85,146 @@ stopCluster(cl)
 stop("End of simulation")
 
 # Merge files -------------------------------------------------------------
-#library(data.table)
+library(data.table)
 
-#obtain colnames of output values
+#algorithms
 algorithms <- c("gpbf", "prodbf", "tbf")
 hyps <- c("_ic", "_iu")
 alg_names <- paste0(rep(algorithms, each = length(hyps)), hyps)
 
 # read in the simulation conditions 
-#res <- as.data.table(readRDS("./Sim_Eli/summarydata.RData"))
 res <- readRDS("./Sim_Eli/summarydata.RData")
 conditions <- colnames(res)
 
 # read in the simulation results
 f <- list.files(path = "./Sim_Eli/Results", pattern = "result", full.names = T)
-tab <- do.call(rbind, lapply(f, function(file){
-  fread(file) #fread_many() is, as of now, beyond my understanding.
-}))
+tab <- setorderv(
+  do.call(rbind, lapply(f, function(file){
+    fread(file) #fread_many() is, as of now, beyond my understanding.
+  })),
+  cols = "V1", order = 1L, na.last = F)
+if(!(tab$V1[1] == 1 & tail(tab$V1, 1) == nrow(res) & length(unique(tab$V1)) == nrow(res))){
+  stop()
+}
+names(tab) <- c("V1", alg_names)
+tab[, "V1" := NULL]
 
 
 # merge conditions and results and omit id variable
-res <- merge(res, tab, by = "V1")
+res<- cbind(res, tab)
 rm(tab)
-colnames(res) <- c(conditions, alg_names)
-res <- res[-1]
 
-# save as .csv and .RData file
-fwrite(res, file.path("./Sim_Eli", paste0("sim_results_", Sys.Date(), ".csv"))) 
-saveRDS(res, file.path("./Sim_Eli", paste0("sim_results_", Sys.Date(), ".RData"))) 
-
-#omit env
-#rm(list=ls())
-res <- readRDS(file.path("./Sim_Eli", paste0("sim_results_", Sys.Date(), ".RData")))
+fwrite(res, file.path("Sim_Eli", paste0("sim_results_", Sys.Date(), ".csv")))
+saveRDS(res, file.path("Sim_Eli", paste0("sim_results_", Sys.Date(), ".RData")))
+f <- list.files("./Sim_Eli/Results", full.names = TRUE)
+file.remove(f)
 
 
+########################
+# Prelimanary Analyses #
+########################
+res <- readRDS(file.path("Sim_Eli", paste0("sim_results_", Sys.Date(), ".RData")))
+lc <- length(conditions)
 
-#######################
-# TRYING fread_many() #
-#######################
-dir <- paste0(getwd(), "/Sim_Eli/Results")
-files <- list.files(path = dir, pattern = "result", full.names = T)
+#obtain unique conditions
+unique_conds <- unique(summarydata[,2:lc])
+unique_conds <- t(apply(unique_conds, 1, function(x){
+  paste(paste0(colnames(unique_conds), " == ", x), collapse = " & ")
+}))
 
+res <- as.data.table(res)
+res[, c(lc:ncol(res)):=lapply(.SD, as.numeric), .SDcols=c(lc:ncol(res))]
 
-fread_many = function(files,header=F,...){
-  if(length(files)==0) return()
-  if(typeof(files)!='character') return()
-  files = files[file.exists(files)]
-  if(length(files)==0) return()
-  tmp = tempfile(fileext = ".csv") #gives names that can be used as temporary files, we specify it to be of .csv extension
-  
-  #I cannot get this part to work and I dont understand it.
-  if(header==T){
-    system(paste0('head -n1 ',files[1],' > ',tmp))
-    system(paste0("xargs awk 'FNR>1' >> ",tmp),input=files)
-  } else {
-    system2(paste0("xargs awk '1' > ",tmp),input=files) #invokes OS command, but I have little OS knowledge
-  }
-  
-  DT = fread(file=tmp,header=header,...)
-  file.remove(tmp)
-  DT
+colfunc <- colorRampPalette(c("red", "blue"))
+colors <- colfunc(16)
+
+library(ggplot2)
+#function to plot the conditions
+plotcond <- function(outcome, name, condition, col){
+  condition <- unlist(strsplit(condition, "&"))
+  condition <- gsub("==", "=", condition)
+  condition <- paste(condition, collapse = " ")
+  propBF3 <- sum(outcome > 3) / length(outcome)
+  hist(outcome, main = condition, col = col,
+       xlab = paste0("median: ", round(median(outcome),2), "; sd: ", round(sd(outcome),2), 
+                     "; prop BF > 3: ", round(propBF3,2)))
+  #legend("topright", condition, cex = 0.8)
 }
-tmp = tempfile(fileext = ".csv")
-setwd(dir = dir)
-tmp <- gsub("\\\\", "/", tempfile(tmpdir = dir, fileext = ".csv"))
-tempdir()
 
-cmd <- paste0("echo", files[1])
+set.seed(6164900)
+sample_cond <- sample(1:length(unique_conds), 16)
+plot16 <- function(name){
+  par(mfrow = c(4,4))
+  for(i in seq_along(sample_cond)){
+    res[eval(parse(text=unique_conds[i])),
+        .(plotcond(eval(parse(text = name)), name, unique_conds[i], col = colors[i]))]
+  }
+}
+
+plot16("gpbf_iu")
+
+
+
+
+
+
+
+
+
+
+
+
+plotcond2 <- function(cond){
+  subcond <- res[eval(parse(text=cond))]
+  outcome <- subcond$gpbf_ic
+  propBF3 <- sum(outcome > 3) / length(outcome)
+  
+  ggplot(subcond, aes(x = gpbf_ic)) +
+    geom_density() +
+    theme_bw() +
+    xlab(paste0("median: ", round(median(outcome),2), "; sd: ", round(sd(outcome),2), 
+                "; prop BF > 3: ", round(propBF3,2))) +
+    legend()
+}
+plotcond2(unique_conds[1])
+
+
+ggplot(tt, aes(x = gpbf_ic))+
+  geom_density()+
+  #geom_vline(data = df_plot[!duplicated(df_plot$Variable), ], aes(xintercept = Median))+
+  #geom_text(data = df_plot[!duplicated(df_plot$Variable), ], aes(label = round(Median), x = Median), y = .2, hjust = -1)+
+  facet_grid(Hypothesis~Condition, scales = "free")+
+  scale_x_log10() +
+  labs(title = "Distribution of Bayes factors for each hypothesis across 100 replications")
+
+apply(sim_conditions[[2]], 1, function(x){
+  x <- na.omit(x)
+  sum(x >3)/length(x)})
+
+
+df_plot <- do.call(rbind, lapply(1:length(sim_conditions), function(thiscond){
+  sim_results <- sim_conditions[[thiscond]]
+  #browser()
+  sapply(1:nrow(sim_results), function(i){
+    prop3 <- na.omit(sim_results[i, ])
+    c(median(sim_results[i, ], na.rm = TRUE), sd(sim_results[i, ], na.rm = TRUE), sum(prop3 >3)/length(prop3))
+  })
+  
+}))
+
+
+tab_power <- data.frame(t(do.call(rbind, lapply(1:length(sim_conditions), function(thiscond){
+  sim_results <- sim_conditions[[thiscond]]
+  #browser()
+  sapply(1:nrow(sim_results), function(i){
+    prop3 <- na.omit(sim_results[i, ])
+    sum(prop3 >3)/length(prop3)
+  })
+  
+}))))
+
+names(tab_power) <- paste0("R = ", c(0, .1, .2, .3))
+tab_power[8:9, ] <- 1-tab_power[8:9, ]
+rownames(tab_power) <- paste0("Hypothesis ", 1:9)
+
+write.csv(tab_power, "tab_power.csv")
--- Sim_Eli/functions.R
+++ Sim_Eli/functions.R
@@ -1,45 +1,46 @@
 #simulate data function
 simdata <- function(es, n, tau2){
-  S <- diag(2) * 1  #initiale covariance matrix
+  S <- diag(2) * 1                  # initiate covariance matrix
   
-  S[row(S) != col(S)] <- es      #correlation between predictor and outcome (off-diagonals)
-  sds <-  c(tau2,1)              #reliability of predictor and outcome, vary outcome
-  S <- diag(sds)%*%S%*%diag(sds) #convert correlation to covariance matrix
+  S[row(S) != col(S)] <- es         # correlation between predictor and outcome (off-diagonals)
+  sds <-  c(tau2,1)                 # reliability of predictor and outcome, vary outcome reliability
+  S <- diag(sds)%*%S%*%diag(sds)    # convert correlation to covariance matrix
   
   df <-  mvrnorm(n, mu = rep(0,2), S, tol = 1E-6, empirical = F) #generate data
-  colnames(df) <- c('y', 'x') #give names
+  colnames(df) <- c('y', 'x')       # give names
   return(df)
 }
+
 
 BFs <- function(es, n, hyp_val, k, tau2){
   
   # create dataframe for each group
   dfs <- lapply(1:k, function(i){
-    n_df <- floor(rnorm(1, n, n/3)) #sample a sample size
-    n_df <- ifelse(n_df < 10, 10, n_df) #make sure sample is at least of size 10
-    simdata(es, n_df, tau2) 
+    n_df <- floor(rnorm(1, n, n/3))     # sample a sample size
+    n_df <- ifelse(n_df < 10, 10, n_df) # make sure sample is at least of size 10
+    simdata(es, n_df, tau2)             # return for every group a dataset
   })
   
-  # obtain estimates for correlations and their standard errors
+  # obtain estimates for correlations and their standard errors for every dataset
   res <- sapply(dfs, function(x){
     est <- cor(x)[,1][-1]                     #estimate for correlation between x and y
-    se_est <- sqrt((1 - est^2)/(nrow(x) - 2)) #standard error = sqrt((1-r^2)/df) with df = N - 2
-    cbind(est, se_est, nrow(x))
+    se_est <- sqrt((1 - est^2)/(nrow(x) - 2)) #estimate for standard error = sqrt((1-r^2)/df) with df = N - 2
+    cbind(est, se_est, nrow(x))               #return estimate, se and sample size of particular set
   })
   
   # necessary naming for bain
   colnames(res) <- paste0('r', 1:k)
   
-  sig <- lapply(res[2,], matrix)   #make list of covariance matrices for the datasets, as shown in the bain vignette
-  ngroup = sapply(dfs, nrow)       #obtain sample size per group
-  names(ngroup) = paste0("r", 1:k) #not sure if necessary, but in bain vignette ngroup is a named vector
-  bf_individual <- lapply(paste0(colnames(res), ">", hyp_val),
-                          bain,
-                          x = res[1,],
-                          Sigma = sig,          #all rho's are assumed to be independent
-                          n = ngroup,
-                          group_parameters = 1, #every group k has 1 parameter which is rho_xy
-                          joint_parameters = 0) #they do not share parameters 
+  sig <- lapply(res[2,], matrix)    # make list of covariance matrices for the datasets, as shown in the bain vignette
+  ngroup <- sapply(dfs, nrow)       # obtain sample size per group
+  names(ngroup) <- paste0("r", 1:k) # not sure if necessary, but in bain vignette ngroup is a named vector
+  bf_individual <- lapply(paste0(colnames(res), ">", hyp_val), # for every group r > 1
+                          bain,                 # call bain
+                          x = res[1,],          # estimates
+                          Sigma = sig,          # all rho's are assumed to be independent
+                          n = ngroup,           # pass the named vector of sample sizes per group to bain
+                          group_parameters = 1, # every group k has 1 parameter which is rho_xy
+                          joint_parameters = 0) # they do not share parameters 
   
   # extract BF_ic and BF_iu for the parameter of every group
   BFs <- t(sapply(bf_individual, function(x){
@@ -47,7 +48,7 @@ BFs <- function(es, n, hyp_val, k, tau2){
   }))
   rm(bf_individual) # bf_individual invokes garbage collection, idk if its faster to use rm() instead of R doing it itself.
   
-  #obtain geometric mean
+  # obtain geometric mean
   gp_and_prod <- apply(BFs, 2, function(x){
     prod_bf <- prod(x)         #obtain product bf
     c(prod_bf^(1/k), prod_bf)  #concatenate geometric product and regular product
@@ -56,10 +57,10 @@ BFs <- function(es, n, hyp_val, k, tau2){
   # create bf_together object to obtain the BFs for the (group1, group2, group3) > hyp_val
   bf_together <- bain(res[1,], 
                       hypothesis = paste0("(", paste0(colnames(res), collapse = ", "), ") > ", hyp_val), 
-                      n = sum(sapply(dfs, nrow)), #should n be total sample size combined? --> infHyp.rmd seems suggest so
-                      Sigma = diag(res[2,]))      #assume independence between groups
+                      n = sum(sapply(dfs, nrow)), # should n be total sample size combined? --> infHyp.rmd seems suggest so
+                      Sigma = diag(res[2,]))      # assume independence between groups
   
-  #returns in order: gpbf_ic, gpbf_iu, prodbf_ic, prodbf_iu, tbf_ic, tbf_iu
+  # returns in order: gpbf_ic, gpbf_iu, prodbf_ic, prodbf_iu, tbf_ic, tbf_iu
   return(c(gp_and_prod[1,], 
               gp_and_prod[2,], 
               c(bf_together$fit$BF.c[1], bf_together$fit$BF.u[1])))
